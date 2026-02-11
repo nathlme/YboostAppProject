bb8c4d099789c60ad5be3cdc0eaf49b7a26e5a94
Lamarche <nathan.lamarche.31@gmail.com>
2026-02-11 18:55:03 +0100

    feat(Handler) : initialize all the handlers

--- cmd/Handler.go
+++ cmd/Handler.go
@@ -49,11 +49,146 @@ func handlerRegister(w http.ResponseWriter, r*http.Request ){
 
 			log.Println("Nouvelle inscription :", email,len(password),pseudo)
 			fmt.Fprint(w, "OK")
-
 			
 		default :
 			fmt.Fprint(w,"erreur 405")
 	} 
 
 	
+}
+
+
+
+
+
+func handlerLogged(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/HomePage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
+}
+
+
+func handlerProfil(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/ProfilPage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
+}
+
+
+func handlerChamps(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/ChampPage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
+}
+
+
+
+func handlerItems(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/ItemsPage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
+}
+
+
+func handlerSearch(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/SearchPage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
+}
+
+
+func handlerNews(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/NewsPage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
+}
+
+func handlerList(w http.ResponseWriter, r*http.Request){
+	switch r.Method {
+		case http.MethodGet :
+			contenu, err := os.ReadFile("templates/ListPage.html")
+
+				if err!=nil {
+					http.Error(w,"Internal Server Error",500)
+					return
+				}
+
+			w.Header().Set("Content-Type", "text/html")
+			w.Write(contenu)
+
+	default :
+			fmt.Fprint(w,"erreur 405")
+	}
 }
--- cmd/main.go
+++ cmd/main.go
@@ -5,22 +5,42 @@ import  (
 	"net/http"
 	"os"
 	"log"
+	"fmt"
 )
 
 
 func main () {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("Hello Yboost 🚀"))
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+			w.Write([]byte("Hello Yboost"))
 		})
 	
+
+	// Handlers
 	http.HandleFunc("/register", handlerRegister)
-		
+	http.HandleFunc("/logged",handlerLogged)
+	http.HandleFunc("/Profil",handlerProfil)
+	http.HandleFunc("/Champs",handlerChamps)
+	http.HandleFunc("/Items",handlerItems)
+	http.HandleFunc("/Search",handlerSearch)
+	http.HandleFunc("/News",handlerNews)
+	http.HandleFunc("/List",handlerList)
+
+
+	
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080" 
 	}
 
 	log.Println("Server running on port", port)
+	fmt.Println("http://localhost:8080/logged")
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
